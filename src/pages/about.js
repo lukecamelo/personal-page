2442cdcc95ee676ccfe8a036a13bfd1cb64f54de
Luke Camelo <azathothbeats@gmail.com>
2018-11-18 02:31:29 -0500

    GraphQL query

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-export default () => (
-  <div>
-    <h1>yes hello</h1>
-    <p>gatsby is so cool</p>
-  </div>
+export default ({ data }) => (
+  <Layout>
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+      <p>gatsby is so cool</p>
+    </div>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
